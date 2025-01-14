6d0f8a166bae28d89731620301baf5bbbcad1215
Dmitry <dmitrbah1@gmail.com>
2025-01-14 17:13:50 +0300

    Added models Recipe

--- recipes_website/main_app/models.py
+++ recipes_website/main_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Recipe(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    name = models.CharField(max_length=30)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_recipes')
+    description = models.CharField(max_length=200)
+    cooking_instruction = models.TextField()
+    cooking_time = models.CharField(max_length=10, blank=True, null=True)
+    published = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+
+    # image =
+    # author =
+
+    class Meta:
+        ordering = ['-published']
+        indexes = [
+            models.Index(fields=['-published'])
+        ]
+
+    def __str__(self):
+        return f"{self.name}"
