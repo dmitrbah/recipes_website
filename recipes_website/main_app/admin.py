65d0e860fbf44856e4ceddb3d77eff3e75b55ce2
Dmitry <dmitrbah1@gmail.com>
2025-01-14 18:15:53 +0300

    Adjusted admin panel

--- recipes_website/main_app/admin.py
+++ recipes_website/main_app/admin.py
@@ -5,3 +5,8 @@ from .models import Recipe
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug', 'author', 'published', 'status']
     prepopulated_fields = {'slug': ('name',)}
+    list_filter = ['status', 'created', 'published', 'author']
+    search_fields = ['name', 'description']
+    raw_id_fields = ['author']
+    date_hierarchy = 'published'
+    ordering = ['status', 'published']
