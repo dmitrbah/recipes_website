daeaeb8acce0486544c91de9b10ccafe84112c6a
Dmitry <dmitrbah1@gmail.com>
2025-01-15 17:37:16 +0300

    Added recipe comments function

--- recipes_website/main_app/urls.py
+++ recipes_website/main_app/urls.py
@@ -6,4 +6,5 @@ app_name = 'main_app'
 urlpatterns = [
     path('', views.recipe_list, name='recipe_list'),
     path('<slug:recipe>/<int:year>/<int:month>/<int:day>/', views.recipe_detail, name='recipe_detail'),
+    path('<int:post_id>/comment', views.post_comment, name='post_comment')
 ]
--- recipes_website/main_app/views.py
+++ recipes_website/main_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Recipe
+from .models import Recipe, Comment
+from .forms import CommentForm
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.views.decorators.http import require_POST
 
 def recipe_list(request):
     recipe_list = Recipe.published_recipes.all()
@@ -21,4 +23,25 @@ def recipe_detail(request, recipe, year, month, day):
                                published__year=year,
                                published__month=month,
                                published__day=day)
-    return render(request, 'main_app/recipe_detail.html', {'recipe': recipe})
+    comments = recipe.comments.filter(active=True)
+    form = CommentForm()
+    return render(request, 'main_app/recipe_detail.html', {'recipe': recipe,
+                                                           'comments': comments,
+                                                           'form': form})
+
+
+@require_POST
+def post_comment(request, post_id):
+    recipe = get_object_or_404(Recipe,
+                               id=post_id,
+                               status=Recipe.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.recipe = recipe
+        comment.save()
+    return render(request, 'main_app/comment.html', {'recipe': recipe,
+                                                     'form': form,
+                                                     'comment': comment})
+
